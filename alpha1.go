f479784f5b24613fdcf9327aeca27661629512ef
Doug Henderson <djhenderson@users.noreply.github.com>
2017-12-27 15:50:10 -0700

    change crlf to lf

--- alpha1.go
+++ alpha1.go
@@ -1,28 +1,30 @@
-  package main
+package main
 
-  import (
-     "github.com/rootsdev/gofamilysearch"
-     "log"
-     "net/http"
-  )
+import (
+	"github.com/rootsdev/gofamilysearch"
+	"log"
+	"net/http"
+)
 
-  func main() {
-      // Context can be shared among go-routines
-     ctx := &gofamilysearch.Context{
-        Environment: "sandbox",
-     }
-     // Client is specific to a user
-     c := &gofamilysearch.Client{
-        Context: ctx,
-        AccessToken: "access token for the requesting user goes here",
-        // if running on app engine, pass in &urlfetch.Transport{Context: appengine.NewContext(request)}
-        Transport: DefaultTransport,
-     }
+func main() {
+	//fs_username := "tum000140053"
+	//fs_password := "1234pass"
 
-     user, err := c.GetCurrentUser()
-     if err != nil {
-        log.Panic(err)
-     }
-     log.Printf("ID=%s personID=%s treeUserID=%s\n", user.ID, user.PersonID, user.TreeUserID)
-  }  
-  
+	// Context can be shared among go-routines
+	ctx := &gofamilysearch.Context{
+		Environment: "sandbox",
+	}
+	// Client is specific to a user
+	c := &gofamilysearch.Client{
+		Context:     ctx,
+		AccessToken: "a0T3000000BtiSAEAZ", /*access token for the requesting user goes here"*/
+		// if running on app engine, pass in &urlfetch.Transport{Context: appengine.NewContext(request)}
+		Transport: http.DefaultTransport,
+	}
+
+	user, err := c.GetCurrentUser()
+	if err != nil {
+		log.Fatalf("GetCurrentUser: %s\n", err)
+	}
+	log.Printf("ID=%s personID=%s treeUserID=%s\n", user.ID, user.PersonID, user.TreeUserID)
+}
